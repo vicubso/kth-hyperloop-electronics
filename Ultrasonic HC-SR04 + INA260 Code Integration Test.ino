70c9099b777fd705cd55ac6a1032f9e36ddda3ec
Gowtham Raj Rangaraju <goutham.raj054@gmail.com>
2022-06-29 15:03:16 +0200

    Create Ultrasonic HC-SR04 + INA260 Code Integration Test.ino
    - Initial commit,
    - Code integrating ultrasonic + voltage sensor.

---
+++ Ultrasonic HC-SR04 + INA260 Code Integration Test.ino
@@ -0,0 +1,59 @@
+#include <Adafruit_INA260.h>
+
+#define echoPin 2 // attach pin D2 Arduino to pin Echo of HC-SR04
+#define trigPin 3 //attach pin D3 Arduino to pin Trig of HC-SR04
+
+// defines variables - Ultrasonic sensor test
+long duration; // variable for the duration of sound wave travel
+int distance; // variable for the distance measurement
+
+Adafruit_INA260 ina260 = Adafruit_INA260();
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+  // Wait until serial port is opened
+  while (!Serial) { delay(10); }
+
+  Serial.println("Adafruit INA260 + Ultrasonic Sensor HC-SR04 Test");
+
+  if (!ina260.begin()) {
+    Serial.println("Couldn't find INA260 chip");
+    while (1);
+  }
+  Serial.println("Found INA260 chip");
+}
+
+void loop() {
+  // Clears the trigPin condition
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+  // Displays the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+  
+  Serial.print("Current: ");
+  Serial.print(ina260.readCurrent());
+  Serial.println(" mA");
+
+  Serial.print("Bus Voltage: ");
+  Serial.print(ina260.readBusVoltage());
+  Serial.println(" mV");
+
+  Serial.print("Power: ");
+  Serial.print(ina260.readPower());
+  Serial.println(" mW");
+
+  Serial.println();
+  delay(1000);
+}
